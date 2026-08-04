fc7b65c6f1c82cf4f2587dabcc13a8b161ac6f3a
srjuniorr <diasslrr@gmail.com>
2026-08-04 17:32:20 -0600

    finish shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 3));
+        shapes.Add(new Rectangle("Blue", 4, 2));
+        shapes.Add(new Circle("Green", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea():F2}.");
+        }
     }
 }
